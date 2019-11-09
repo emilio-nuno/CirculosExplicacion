bf2d5f2a8657d8c4510520cfb5cb2a2b7b79d769
Emilio Nuño Paz <superbob_99@hotmail.com>
2019-11-09 11:03:37 -0800

    Removidas funciones de Etapa 3

--- CirculosExplicacion/Form1.Designer.cs
+++ CirculosExplicacion/Form1.Designer.cs
@@ -42,17 +42,16 @@
             this.botonOrdenar = new System.Windows.Forms.Button();
             this.listaAntes = new System.Windows.Forms.ListBox();
             this.listaDespues = new System.Windows.Forms.ListBox();
-            this.botonAnimar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.selectedImage)).BeginInit();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // selectedImage
             // 
-            this.selectedImage.Location = new System.Drawing.Point(9, 9);
-            this.selectedImage.Margin = new System.Windows.Forms.Padding(2);
+            this.selectedImage.Location = new System.Drawing.Point(12, 11);
+            this.selectedImage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.selectedImage.Name = "selectedImage";
-            this.selectedImage.Size = new System.Drawing.Size(490, 395);
+            this.selectedImage.Size = new System.Drawing.Size(653, 486);
             this.selectedImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.selectedImage.TabIndex = 0;
             this.selectedImage.TabStop = false;
@@ -63,10 +62,10 @@
             // 
             // botonSelect
             // 
-            this.botonSelect.Location = new System.Drawing.Point(529, 9);
-            this.botonSelect.Margin = new System.Windows.Forms.Padding(2);
+            this.botonSelect.Location = new System.Drawing.Point(705, 11);
+            this.botonSelect.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.botonSelect.Name = "botonSelect";
-            this.botonSelect.Size = new System.Drawing.Size(112, 41);
+            this.botonSelect.Size = new System.Drawing.Size(149, 50);
             this.botonSelect.TabIndex = 1;
             this.botonSelect.Text = "Seleccionar Imagen";
             this.botonSelect.UseVisualStyleBackColor = true;
@@ -74,10 +73,10 @@
             // 
             // botonFindCenter
             // 
-            this.botonFindCenter.Location = new System.Drawing.Point(529, 53);
-            this.botonFindCenter.Margin = new System.Windows.Forms.Padding(2);
+            this.botonFindCenter.Location = new System.Drawing.Point(705, 65);
+            this.botonFindCenter.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.botonFindCenter.Name = "botonFindCenter";
-            this.botonFindCenter.Size = new System.Drawing.Size(112, 41);
+            this.botonFindCenter.Size = new System.Drawing.Size(149, 50);
             this.botonFindCenter.TabIndex = 2;
             this.botonFindCenter.Text = "Encontrar Centros";
             this.botonFindCenter.UseVisualStyleBackColor = true;
@@ -86,50 +85,52 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(660, 25);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(880, 31);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(113, 69);
+            this.listBox1.Size = new System.Drawing.Size(149, 84);
             this.listBox1.TabIndex = 3;
             // 
             // nodosConectados
             // 
             this.nodosConectados.ContextMenuStrip = this.contextMenuStrip1;
-            this.nodosConectados.Location = new System.Drawing.Point(660, 98);
-            this.nodosConectados.Margin = new System.Windows.Forms.Padding(2);
+            this.nodosConectados.Location = new System.Drawing.Point(880, 121);
+            this.nodosConectados.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.nodosConectados.Name = "nodosConectados";
-            this.nodosConectados.Size = new System.Drawing.Size(113, 80);
+            this.nodosConectados.Size = new System.Drawing.Size(149, 98);
             this.nodosConectados.TabIndex = 4;
             this.nodosConectados.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.nodosConectados_NodeMouseClick);
             // 
             // contextMenuStrip1
             // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.origenToolStripMenuItem,
             this.destinoToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(130, 52);
             // 
             // origenToolStripMenuItem
             // 
             this.origenToolStripMenuItem.Name = "origenToolStripMenuItem";
-            this.origenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.origenToolStripMenuItem.Size = new System.Drawing.Size(129, 24);
             this.origenToolStripMenuItem.Text = "Origen";
             this.origenToolStripMenuItem.Click += new System.EventHandler(this.origenToolStripMenuItem_Click);
             // 
             // destinoToolStripMenuItem
             // 
             this.destinoToolStripMenuItem.Name = "destinoToolStripMenuItem";
-            this.destinoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.destinoToolStripMenuItem.Size = new System.Drawing.Size(129, 24);
             this.destinoToolStripMenuItem.Text = "Destino";
             this.destinoToolStripMenuItem.Click += new System.EventHandler(this.destinoToolStripMenuItem_Click);
             // 
             // botonConectar
             // 
-            this.botonConectar.Location = new System.Drawing.Point(529, 98);
-            this.botonConectar.Margin = new System.Windows.Forms.Padding(2);
+            this.botonConectar.Location = new System.Drawing.Point(705, 121);
+            this.botonConectar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.botonConectar.Name = "botonConectar";
-            this.botonConectar.Size = new System.Drawing.Size(112, 41);
+            this.botonConectar.Size = new System.Drawing.Size(149, 50);
             this.botonConectar.TabIndex = 5;
             this.botonConectar.Text = "Conectar";
             this.botonConectar.UseVisualStyleBackColor = true;
@@ -137,10 +138,10 @@
             // 
             // botonOrdenar
             // 
-            this.botonOrdenar.Location = new System.Drawing.Point(529, 144);
-            this.botonOrdenar.Margin = new System.Windows.Forms.Padding(2);
+            this.botonOrdenar.Location = new System.Drawing.Point(705, 177);
+            this.botonOrdenar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.botonOrdenar.Name = "botonOrdenar";
-            this.botonOrdenar.Size = new System.Drawing.Size(112, 41);
+            this.botonOrdenar.Size = new System.Drawing.Size(149, 50);
             this.botonOrdenar.TabIndex = 6;
             this.botonOrdenar.Text = "Ordenar Vértices";
             this.botonOrdenar.UseVisualStyleBackColor = true;
@@ -149,37 +150,28 @@
             // listaAntes
             // 
             this.listaAntes.FormattingEnabled = true;
-            this.listaAntes.Location = new System.Drawing.Point(528, 270);
-            this.listaAntes.Margin = new System.Windows.Forms.Padding(2);
+            this.listaAntes.ItemHeight = 16;
+            this.listaAntes.Location = new System.Drawing.Point(704, 332);
+            this.listaAntes.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listaAntes.Name = "listaAntes";
-            this.listaAntes.Size = new System.Drawing.Size(113, 69);
+            this.listaAntes.Size = new System.Drawing.Size(149, 84);
             this.listaAntes.TabIndex = 7;
             // 
             // listaDespues
             // 
             this.listaDespues.FormattingEnabled = true;
-            this.listaDespues.Location = new System.Drawing.Point(659, 270);
-            this.listaDespues.Margin = new System.Windows.Forms.Padding(2);
+            this.listaDespues.ItemHeight = 16;
+            this.listaDespues.Location = new System.Drawing.Point(879, 332);
+            this.listaDespues.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listaDespues.Name = "listaDespues";
-            this.listaDespues.Size = new System.Drawing.Size(113, 69);
+            this.listaDespues.Size = new System.Drawing.Size(149, 84);
             this.listaDespues.TabIndex = 8;
-            // 
-            // botonAnimar
-            // 
-            this.botonAnimar.Location = new System.Drawing.Point(528, 190);
-            this.botonAnimar.Name = "botonAnimar";
-            this.botonAnimar.Size = new System.Drawing.Size(113, 41);
-            this.botonAnimar.TabIndex = 9;
-            this.botonAnimar.Text = "Animar";
-            this.botonAnimar.UseVisualStyleBackColor = true;
-            this.botonAnimar.Click += new System.EventHandler(this.botonAnimar_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(807, 447);
-            this.Controls.Add(this.botonAnimar);
+            this.ClientSize = new System.Drawing.Size(1076, 550);
             this.Controls.Add(this.listaDespues);
             this.Controls.Add(this.listaAntes);
             this.Controls.Add(this.botonOrdenar);
@@ -189,7 +181,7 @@
             this.Controls.Add(this.botonFindCenter);
             this.Controls.Add(this.botonSelect);
             this.Controls.Add(this.selectedImage);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.selectedImage)).EndInit();
@@ -213,6 +205,5 @@
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem origenToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem destinoToolStripMenuItem;
-        private System.Windows.Forms.Button botonAnimar;
     }
 }
--- CirculosExplicacion/Form1.cs
+++ CirculosExplicacion/Form1.cs
@@ -20,22 +20,18 @@ namespace CirculosExplicacion //TODO: CAMBIAR EL SORT A EL MAYOR DE LOS DOS RADI
 
     public partial class Form1 : Form
     {//Usar listbox
-        private int señuelos = 3;
-        private int contador = 0;
         private Bitmap originalImage;
         private Dictionary<int, Tuple<int, int, int>> centros;
         private Dictionary<int, List<Dictionary<int, Tuple<int, int, int>>>> conexiones;
         private bool sobreescribir;
         private Circle primera;
         private Grafo g;
-        private Señuelo señuelo;
         private Dictionary<int, Dictionary<int, List<Tuple<int, int>>>> caminos;
         private List<Agente> agentes;
-        private bool encontrado;
+        private Señuelo señuelo;
 
         public Form1()
         {
-            this.encontrado = false;
             this.agentes = new List<Agente>();
             InitializeComponent();
             this.sobreescribir = false;
@@ -121,167 +117,6 @@ namespace CirculosExplicacion //TODO: CAMBIAR EL SORT A EL MAYOR DE LOS DOS RADI
             ((TreeView)sender).SelectedNode = e.Node;
         }
 
-        private bool Caminar(Agente agente, int destino, Bitmap bmp)
-        {
-            if(agente.Velocidad + agente.Pos < caminos[agente.Actual][destino].Count - 1)
-            {
-                selectedImage.BackgroundImage = originalImage;
-                selectedImage.BackgroundImageLayout = ImageLayout.Zoom; //Para que encuadre
-                DibujarCirculo(caminos[agente.Actual][destino][agente.Pos].Item1, caminos[agente.Actual][destino][agente.Pos].Item2, bmp, 10, agente.Color);
-                DibujarRadar(caminos[agente.Actual][destino][agente.Pos].Item1, caminos[agente.Actual][destino][agente.Pos].Item2, bmp, 10, agente.Color);
-                agente.Pos += agente.Velocidad;
-                return true;
-            }
-            else
-            {
-                DibujarCirculo(caminos[agente.Actual][destino][caminos[agente.Actual][destino].Count-1].Item1, caminos[agente.Actual][destino][caminos[agente.Actual][destino].Count - 1].Item2, bmp, 10, agente.Color);
-                DibujarRadar(caminos[agente.Actual][destino][agente.Pos].Item1, caminos[agente.Actual][destino][agente.Pos].Item2, bmp, 10, agente.Color);
-                agente.Pos = 0;
-                return false;
-            }
-        }
-
-        private int Buscar_Camino(Agente a, Dictionary<int, Dictionary<int, Dictionary<int, int>>> visitados)
-        {
-            if (a.Actual == señuelo.Actual)
-            {
-                encontrado = true;
-                SeñueloAleatorio();
-                a.Velocidad += 10;
-            }
-
-            Dictionary<int, double> prioridad = new Dictionary<int, double>();
-            foreach (int id in caminos[a.Actual].Keys)
-            {
-                double thetaBait = Math.Atan2((señuelo.Y - caminos[a.Actual][id][0].Item2), (señuelo.X - caminos[a.Actual][id][0].Item1));
-                double thetaEdge = Math.Atan2((caminos[a.Actual][id][caminos[a.Actual][id].Count-1].Item2 - caminos[a.Actual][id][0].Item2), (caminos[a.Actual][id][caminos[a.Actual][id].Count - 1].Item1 - caminos[a.Actual][id][0].Item1));
-                if((thetaEdge > Math.PI) && (thetaBait == 0))
-                {
-                    thetaBait = (2 * Math.PI);
-                }
-                double tempdiff = Math.Abs(thetaBait - thetaEdge);
-                prioridad.Add(id, tempdiff);
-            }
-
-            List<double> ordenado = prioridad.Values.ToList();
-            var destino = from entry in prioridad where entry.Value == ordenado.Min() select entry.Key;
-            int destinoactual = destino.FirstOrDefault();
-
-            if (visitados[a.Inicial][a.Actual][destinoactual] == 0)
-            {
-                //visitados[a.Inicial][a.Actual][destinoactual] += 1;
-                return destinoactual;
-            }
-            else
-            {
-                int id = visitados[a.Inicial][a.Actual].Aggregate((l, r) => l.Value < r.Value ? l : r).Key;
-                return id;
-            }
-        }
-
-        private void SeñueloAleatorio()
-        {
-            Random aleatorio = new Random();
-            int index = aleatorio.Next(centros.Count);
-            señuelo.Actual = index;
-            //Next two lines probably useless
-            señuelo.X = centros[index].Item1;
-            señuelo.Y = centros[index].Item2;
-        }
-
-        private void DarPaso(Dictionary<int, Dictionary<int, int>> destinos) //a.Actual y destino
-        {
-            List<int> terminados = new List<int>();
-            int contadorTerminado = 0;
-            using (Bitmap bmp = new Bitmap(originalImage))
-            {
-                while (contadorTerminado < agentes.Count)
-                {
-                    foreach (Agente agente in agentes)
-                    {
-                        if (!terminados.Contains(agente.Inicial))
-                        {
-                            if (!Caminar(agente, destinos[agente.Inicial][agente.Actual], bmp))
-                            {
-                                terminados.Add(agente.Inicial);
-                                contadorTerminado++;
-                            }
-                        }
-                    }
-                    selectedImage.Image = bmp;
-                    selectedImage.Refresh();
-                    Thread.Sleep(1);
-                }
-            }
-        }
-
-        private void botonAnimar_Click(object sender, EventArgs e) //Actualmente el primer agente llega al señuelo seleccionado, y los demas llegan a un señuelo random
-        {
-            agentes[0].Color = Color.Red;
-            //agentes[1].Color = Color.Green;
-            //agentes[2].Color = Color.Pink;
-            //agentes[3].Color = Color.Yellow;
-
-            Dictionary<int, Dictionary<int, Dictionary<int, int>>> visitados = new Dictionary<int, Dictionary<int, Dictionary<int, int>>>();
-            foreach (Agente agente in agentes)
-            {
-                Dictionary<int, Dictionary<int, int>> aux = new Dictionary<int, Dictionary<int, int>>();
-                agente.Velocidad = 10;
-                foreach (int id in caminos.Keys)
-                {
-                    Dictionary<int, int> temp = new Dictionary<int, int>();
-                    foreach (int conectado in caminos[id].Keys)
-                    {
-                        temp.Add(conectado, 0);
-                    }
-                    aux.Add(id, temp);
-                }
-                visitados.Add(agente.Inicial, aux);
-            }
-
-            while (contador < señuelos)
-            {
-                encontrado = false;
-                Dictionary<int, Dictionary<int, int>> destinos = new Dictionary<int, Dictionary<int, int>>();
-                while (!encontrado)
-                {
-                    foreach (Agente agente in agentes)
-                    {
-                        if (Existe_Camino(agente.Actual, señuelo.Actual))
-                        {
-                            //Agregar velocidad a agente actual, solo aumentamos velocidad en el metodo de buscar
-                            destinos.Add(agente.Inicial, new Dictionary<int, int>() { { agente.Actual, señuelo.Actual } });
-                            SeñueloAleatorio();
-                            encontrado = true;
-                            agente.Velocidad += 10;
-                        }
-                        else
-                        {
-                            int temp = Buscar_Camino(agente, visitados);
-                            destinos.Add(agente.Inicial, new Dictionary<int, int>() { { agente.Actual, temp } });
-                            visitados[agente.Inicial][agente.Actual][temp] += 1; //La visita la haces dos veces
-                        }
-                    }
-                    DarPaso(destinos);
-                    foreach(Agente agente in agentes)
-                    {
-                        foreach (int id in destinos.Keys)
-                        {
-                            foreach (int actual in destinos[id].Keys)
-                            {
-                                if(agente.Inicial == id)
-                                {
-                                    agente.Actual = destinos[agente.Inicial][agente.Actual];
-                                }
-                        }
-                        }
-                    }
-                    destinos.Clear();
-                }
-                contador++;
-            }
-        }
-
         private void origenToolStripMenuItem_Click(object sender, EventArgs e)
         {
             agentes.Add(new Agente(centros[Int32.Parse(nodosConectados.SelectedNode.Text)].Item1, centros[Int32.Parse(nodosConectados.SelectedNode.Text)].Item2, 50, Int32.Parse(nodosConectados.SelectedNode.Text), Int32.Parse(nodosConectados.SelectedNode.Text), Color.Transparent)); //Agregar color manual
@@ -299,36 +134,6 @@ namespace CirculosExplicacion //TODO: CAMBIAR EL SORT A EL MAYOR DE LOS DOS RADI
                 graphics.Clear(Color.Transparent);
                 graphics.FillEllipse(new SolidBrush(color), x - (radio / 2), y - (radio / 2), radio, radio);
             }
-        }
-
-        private void DibujarSeñuelo(int x, int y, Bitmap bmp, int radio, Color color)
-        {
-            using (var graphics = Graphics.FromImage(bmp))
-            {
-                graphics.FillEllipse(new SolidBrush(color), x - (radio / 2), y - (radio / 2), radio, radio);
-            }
-        }
-
-        private void DibujarRadar(int x, int y, Bitmap bmp, int largo, Color color)
-        {
-            using (var graphics = Graphics.FromImage(bmp))
-            {
-                Pen p = new Pen(color, largo);
-                p.StartCap = System.Drawing.Drawing2D.LineCap.Round;
-                p.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
-
-                double theta = Math.Atan2(señuelo.Y - y, señuelo.X - x);
-                int flechax = (int)Math.Round(40 * Math.Cos(theta) + x);
-                int flechay = (int)Math.Round(40 * Math.Sin(theta) + y);
-
-                graphics.Clear(Color.Transparent);
-                graphics.DrawLine(p, x, y, flechax, flechay);
-            }
-        }
-
-        private bool Existe_Camino(int origen, int destino) //Se trabaja con IDs de vértice
-        {
-            return caminos[origen].ContainsKey(destino) ? true : false;
         }
     }
 }
